ed3b84adf1d84b84d2efabe72ecc1e0084cb9db7
justinekoa <justinekoa@gmail.com>
2020-03-02 22:40:09 -0800

    button to go back to current mood page from recommendation page

--- client/lib/screens/currentMood/recommendation.dart
+++ client/lib/screens/currentMood/recommendation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import './currentMood.dart';
 
 // TODO format recommendation text
 class Recommendation extends StatelessWidget {
@@ -14,7 +15,31 @@ class Recommendation extends StatelessWidget {
         middle: Text('Get Happy Recommendation'),
       ),
       child: Center(
-        child: Text(_recommendation),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              child: Center(
+                child: Text(_recommendation),
+              ),
+            ),
+            CupertinoButton(
+              minSize: 65,
+              padding: EdgeInsets.zero,
+              borderRadius: BorderRadius.circular(35.0),
+              onPressed: () {
+                // TODO make this transition less ugly
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+              },
+              color: Colors.blue,
+              child: Icon(
+                Icons.add,
+                size: 40.0,
+              ),
+            ),
+          ],
+        ),
       ),
       backgroundColor: CupertinoColors.lightBackgroundGray,
     );
