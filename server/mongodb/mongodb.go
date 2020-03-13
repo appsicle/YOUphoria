57580f1850ec796f54342257e0def917d5375583
Leon <leonlin.48@gmail.com>
2020-03-13 16:32:52 -0700

    removing mongo creds from code

--- server/mongodb/mongodb.go
+++ server/mongodb/mongodb.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"time"
 	"context"
+	"os"
+
     "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Client *mongo.Client
@@ -13,12 +15,12 @@ var ProfileCollection *mongo.Collection
 var TokenCollection *mongo.Collection
 
 func ConnectDB() {
-	fmt.Println("Starting the application...")
+	// fmt.Println("Starting the application...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	clientOptions := options.Client().ApplyURI("mongodb+srv://testacc:testaccpw@youphoria-r8kiz.mongodb.net/test?retryWrites=true&w=majority")
-	
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGOURI"))
+
 	Client, _ = mongo.Connect(ctx, clientOptions)
 	ProfileCollection = Client.Database("YOUphoria").Collection("profiles")
 	TokenCollection = Client.Database("YOUphoria").Collection("tokens")
